e71d8ed2402548d0cbfd7639cba22c9753eef5e4
yuanqing <desk@yuanqing.sg>
2019-07-09 23:28:01 +0800

    Reify

---
+++ index.js
@@ -0,0 +1,43 @@
+const Color = require('color')
+const postcss = require('postcss')
+
+const hexRegex = require('hex-color-regex')()
+const hslRegex = require('hsl-regex')()
+const hslaRegex = require('hsla-regex')()
+const rgbRegex = require('rgb-regex')()
+const rgbaRegex = require('rgba-regex')()
+
+const postcssTransformColor = postcss.plugin(
+  'postcss-transform-color',
+  function () {
+    return function (root) {
+      root.walkDecls(function (decl) {
+        let value = decl.value
+        value = invertColor(value, hexRegex, hex)
+        value = invertColor(value, rgbRegex, rgb)
+        value = invertColor(value, rgbaRegex, rgb)
+        value = invertColor(value, hslRegex, hsl)
+        value = invertColor(value, hslaRegex, hsl)
+        decl.value = value
+      })
+    }
+  }
+)
+
+function invertColor (value, regexp, transform) {
+  return value.replace(regexp, function (match) {
+    return transform(new Color(match).negate()).toLowerCase()
+  })
+}
+
+function hex (value) {
+  return value.hex()
+}
+function hsl (value) {
+  return value.hsl().string()
+}
+function rgb (value) {
+  return value.rgb().string()
+}
+
+module.exports = postcssTransformColor
---
+++ test.js
@@ -0,0 +1,55 @@
+const postcss = require('postcss')
+const test = require('tape')
+
+const postcssTransformColor = require('./')
+
+function run (input, output) {
+  return async function (t) {
+    t.plan(2)
+    const result = await postcss([postcssTransformColor()]).process(input, {
+      from: null
+    })
+    t.equal(result.css, output)
+    t.equal(result.warnings().length, 0)
+  }
+}
+
+test(
+  'hex',
+  run(
+    'a {border-bottom: 1px solid #ffffff; }',
+    'a {border-bottom: 1px solid #000000; }'
+  )
+)
+
+test(
+  'rgb',
+  run(
+    'a {border-bottom: 1px solid rgb(255, 255, 255); }',
+    'a {border-bottom: 1px solid rgb(0, 0, 0); }'
+  )
+)
+
+test(
+  'rgba',
+  run(
+    'a {border-bottom: 1px solid rgba(255, 255, 255, 0); }',
+    'a {border-bottom: 1px solid rgba(0, 0, 0, 0); }'
+  )
+)
+
+test(
+  'hsl',
+  run(
+    'a {border-bottom: 1px solid hsl(0, 0%, 100%); }',
+    'a {border-bottom: 1px solid hsl(0, 0%, 0%); }'
+  )
+)
+
+test(
+  'hsla',
+  run(
+    'a {border-bottom: 1px solid hsla(0, 0%, 100%, 0); }',
+    'a {border-bottom: 1px solid hsla(0, 0%, 0%, 0); }'
+  )
+)
